dabb3244a2c93027ab0fe1ce3968010b3cb66503
HiroyukiSakoh <38931510+HiroyukiSakoh@users.noreply.github.com>
2022-02-09 00:41:41 +0900

    add repro

---
+++ ReproEdgeIEModeParallel/Program.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace ReproEdgeIEModeParallel
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //IE 
+            {
+                using var driverA = OpenIE(logfile: "IE-A.log");
+                driverA.Navigate().GoToUrl("https://github.com/");
+
+                Console.WriteLine(driverA.Title);//Github
+
+                using var driverB = OpenIE(logfile: "IE-B.log");
+                driverB.Navigate().GoToUrl("https://bing.com/");
+
+                Console.WriteLine(driverA.Title);//Github
+                Console.WriteLine(driverB.Title);//Bing
+
+                driverA.Quit();
+                driverB.Quit();
+            }
+
+            //Edge(IEMode)
+            {
+                using var driverA = OpenEdgeIEMode(logfile: "EdgeIE-A.log");
+                driverA.Navigate().GoToUrl("https://Github.com/");
+
+                Console.WriteLine(driverA.Title);//Github
+
+                using var driverB = OpenEdgeIEMode(logfile: "EdgeIE-B.log");
+                driverB.Navigate().GoToUrl("https://bing.com/");
+
+                Console.WriteLine(driverA.Title);//Bing ****
+                Console.WriteLine(driverB.Title);//Bing
+
+                driverA.Quit();
+                driverB.Quit();//slowly ***
+            }
+        }
+
+        private static WebDriver OpenEdgeIEMode(string logfile)
+        {
+            InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+            ieOptions.AttachToEdgeChrome = true;
+            ieOptions.EdgeExecutablePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Microsoft\\Edge\\Application\\msedge.exe");
+            var ieService = InternetExplorerDriverService.CreateDefaultService();
+            var ieLogFile = Path.Combine(Directory.GetCurrentDirectory(), logfile);
+            ieService.LogFile = ieLogFile;
+            ieService.LoggingLevel = InternetExplorerDriverLogLevel.Trace;
+            var driver = new InternetExplorerDriver(ieService, ieOptions, TimeSpan.FromSeconds(10));
+            return driver;
+        }
+        private static WebDriver OpenIE(string logfile)
+        {
+            InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+            var ieService = InternetExplorerDriverService.CreateDefaultService();
+            var ieLogFile = Path.Combine(Directory.GetCurrentDirectory(), logfile);
+            ieService.LogFile = ieLogFile;
+            ieService.LoggingLevel = InternetExplorerDriverLogLevel.Trace;
+            var driver = new InternetExplorerDriver(ieService, ieOptions, TimeSpan.FromSeconds(10));
+            return driver;
+        }
+    }
+}
